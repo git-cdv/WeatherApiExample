ea604735c7706b5e61bb078487a2b29659500250
chkan <chkan.dv@gmail.com>
2024-01-26 14:57:33 +0200

    implemented themes to Compose

--- app/src/main/java/chkan/example/weatherapiexample/MainActivity.kt
+++ app/src/main/java/chkan/example/weatherapiexample/MainActivity.kt
@@ -1,9 +1,9 @@
 package chkan.example.weatherapiexample
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -12,30 +12,33 @@ import androidx.navigation.navArgument
 import chkan.example.weatherapiexample.ui.ShareViewModel
 import chkan.example.weatherapiexample.ui.screens.details.DetailsScreen
 import chkan.example.weatherapiexample.ui.screens.list.WeatherListScreen
+import chkan.example.weatherapiexample.ui.theme.AppCustomTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(){
+class MainActivity : ComponentActivity(){
 
     private val shareViewModel: ShareViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
+            AppCustomTheme {
+                val navController = rememberNavController()
 
-            NavHost(navController = navController, startDestination = "list") {
-                composable("list") {
-                    WeatherListScreen(onNavigateToDetails = { navController.navigate("details/$it") }, shareViewModel = shareViewModel)
-                }
-                composable(
-                    route = "details/{city}",
-                    arguments = listOf(navArgument("city") { type = NavType.StringType })
-                ) {
-                    val city = it.arguments?.getString("city")
-                    city?.let {
-                        val forecastList = shareViewModel.getForecastDataByCity(city) ?: listOf()
-                        DetailsScreen(city = city, onNavigateBack = { navController.popBackStack() }, forecastList = forecastList)
+                NavHost(navController = navController, startDestination = "list") {
+                    composable("list") {
+                        WeatherListScreen(onNavigateToDetails = { navController.navigate("details/$it") }, shareViewModel = shareViewModel)
+                    }
+                    composable(
+                        route = "details/{city}",
+                        arguments = listOf(navArgument("city") { type = NavType.StringType })
+                    ) {
+                        val city = it.arguments?.getString("city")
+                        city?.let {
+                            val forecastList = shareViewModel.getForecastDataByCity(city) ?: listOf()
+                            DetailsScreen(city = city, onNavigateBack = { navController.popBackStack() }, forecastList = forecastList)
+                        }
                     }
                 }
             }
